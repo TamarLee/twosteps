6b2d7f549831578c4de9ad8affe707a109e25166
DESKTOP-IVAB5BU\user <tmekhri@gmail.com>
2018-02-11 15:56:33 +0400

    add ფუნქცია ადმინკისთვის

---
+++ resources/views/admin/products/products.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<style type="text/css">
+		td {
+			width: 50px;
+			height: 50px;
+			border: solid 2px green;
+		}
+
+	</style>
+</head>
+<body>
+	<table>
+		<tr>
+			<td></td>
+			<td></td>
+			<td></td>
+		</tr>
+		<tr>
+			<td></td>
+			<td></td>
+			<td></td>
+		</tr>
+	</table>
+</body>
+</html>
